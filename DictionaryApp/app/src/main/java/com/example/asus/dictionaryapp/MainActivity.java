42852ea08b078d8c1d33e0ad08ff6668776e82da
namtan <namtan8888@gmail.com>
2016-03-09 10:59:47 +0700

    Iteration 1 - UI main

---
+++ DictionaryApp/app/src/main/java/com/example/asus/dictionaryapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.asus.dictionaryapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageButton addButton, editButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initComponents();
+    }
+
+    private void initComponents() {
+        addButton = (ImageButton) findViewById(R.id.addButton);
+        editButton = (ImageButton) findViewById(R.id.editButton);
+    }
+}
