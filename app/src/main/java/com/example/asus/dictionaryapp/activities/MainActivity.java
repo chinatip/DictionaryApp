58481d70a3447985a702008da659d0d3fd101021
chinatip <chinatip.v@ku.th>
2016-04-11 15:30:01 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/asus/dictionaryapp/activities/MainActivity.java
+++ app/src/main/java/com/example/asus/dictionaryapp/activities/MainActivity.java
@@ -1,141 +1,49 @@
 /**
  * Created by Chinatip Vichian 5710546551
  */
-package com.example.asus.dictionaryapp.activities;
+package com.example.asus.dictionaryapp.adapters;
 
+import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.os.Build;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ListView;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.example.asus.dictionaryapp.R;
-import com.example.asus.dictionaryapp.adapters.WordListAdapter;
 import com.example.asus.dictionaryapp.model.Word;
-import com.example.asus.dictionaryapp.util.Storage;
-
-import org.json.JSONException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    private ImageButton addButton, editButton;
-    private EditText searchText;
-    public ListView lv;
-    private static WordListAdapter wordsAdapter;
-    private ArrayList<Word> words;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initComponents();
+public class WordListAdapter extends ArrayAdapter<Word> {
+    public WordListAdapter(Context context, int resource, ArrayList<Word> objects){
+        super(context, resource, objects);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        loadWords();
-    }
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
 
-    private void initComponents() {
-        lv = (ListView)findViewById(R.id.wordListView);
-        words = new ArrayList<Word>();
-        addButton = (ImageButton) findViewById(R.id.addButton);
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AddWordActivity.class);
-                startActivity(intent);
-            }
-        });
-        editButton = (ImageButton) findViewById(R.id.editButton);
-        editButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, EditWordListActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        wordsAdapter = new WordListAdapter(this, R.layout.word_list, words);
-        lv.setAdapter(wordsAdapter);
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
-                Intent intent = new Intent(MainActivity.this, WordActivity.class);
-                intent.putExtra("word", words.get(i));
-                startActivity(intent);
-
-            }
-        });
-        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-
-            public boolean onItemLongClick(AdapterView<?> arg0, View v, int i, long arg3) {
-                Word w = words.get(i);
-                if (w.getIsPinned()) {
-                    w.setIsChecked(false);
-                    wordsAdapter.setState(1);
-                    wordsAdapter.notifyDataSetChanged();
-                } else {
-                    w.setIsChecked(true);
-                    wordsAdapter.setState(0);
-                    wordsAdapter.notifyDataSetChanged();
-                }
-                return true;
-            }
-        });
-        searchText = (EditText) findViewById(R.id.searchText);
-        searchText.addTextChangedListener(new TextWatcher() {
-            public void afterTextChanged(Editable arg0) {
-                ArrayList<Word> src_list = new ArrayList<Word>();
-                int textlength = searchText.getText().length();
-                for (int i = 0; i < words.size(); i++) {
-                    try {
-                        if (searchText.getText().toString()
-                                .equalsIgnoreCase(words.get(i).getWord()
-                                        .subSequence(0, textlength)
-                                        .toString())) {
-                            src_list.add(words.get(i));
-                        }
-                    } catch (Exception e) {
-                    }
-                }
-                lv.setAdapter(new WordListAdapter(MainActivity.this
-                        , R.layout.word_list
-                        , src_list));
-            }
-
-            public void beforeTextChanged(CharSequence s, int start
-                    , int count, int after) {
-            }
-
-            public void onTextChanged(CharSequence s, int start
-                    , int before, int count) { }
-
-        });
-    }
-
-    public void loadWords() {
-        try {
-            words.clear();
-            for(Word n: Storage.getInstance().loadWords(this)) {
-                words.add(n);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
+        if (v == null) {
+            LayoutInflater vi;
+            vi = LayoutInflater.from(getContext());
+            v = vi.inflate(R.layout.word_list, null);
         }
-    }
-    public static void update() {
-        wordsAdapter.notifyDataSetChanged();
+        TextView word = (TextView) v.findViewById(R.id.word);
+        word.setText(getItem(position).getWord());
+
+        if (getItem(position).getIsPinned())
+            v.setBackgroundColor(Color.YELLOW);
+        else
+            v.setBackgroundColor(Color.WHITE);// to change the word cell color
+        return v;
     }
 
 }
