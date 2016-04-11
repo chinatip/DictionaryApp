ee79b9478167c26cf5e7eba41c7330098fdb003c
chinatip <chinatip.v@ku.th>
2016-04-11 15:31:20 +0700

    Update WordListAdapter.java

--- app/src/main/java/com/example/asus/dictionaryapp/adapters/WordListAdapter.java
+++ app/src/main/java/com/example/asus/dictionaryapp/adapters/WordListAdapter.java
@@ -3,8 +3,11 @@
  */
 package com.example.asus.dictionaryapp.adapters;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,35 +18,32 @@ import com.example.asus.dictionaryapp.R;
 import com.example.asus.dictionaryapp.model.Word;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
 public class WordListAdapter extends ArrayAdapter<Word> {
-    int state =0;
-    //0 - not pin
-    //1 - pin
     public WordListAdapter(Context context, int resource, ArrayList<Word> objects){
         super(context, resource, objects);
     }
 
-    public View getView(int position, View convertView, ViewGroup parent){
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public View getView(int position, View convertView, ViewGroup parent) {
         View v = convertView;
 
-        if(v == null) {
+        if (v == null) {
             LayoutInflater vi;
             vi = LayoutInflater.from(getContext());
             v = vi.inflate(R.layout.word_list, null);
         }
         TextView word = (TextView) v.findViewById(R.id.word);
         word.setText(getItem(position).getWord());
-        if(state==1)
-            word.setTextColor(Color.RED);
+
+        if (getItem(position).getIsPinned())
+            v.setBackgroundColor(Color.YELLOW);
         else
-            word.setTextColor(Color.GRAY);
+            v.setBackgroundColor(Color.WHITE);// to change the word cell color
         return v;
     }
 
-    public void setState(int state) {
-        this.state = state;
-    }
 }
